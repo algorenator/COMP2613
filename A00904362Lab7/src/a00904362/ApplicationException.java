1a745ca59de01206c54e4641fd4bf493a94b1644
AlexeyGorbenko <algorenator@gmail.com>
2016-02-29 14:25:53 -0800

    initial lab7

---
+++ A00904362Lab7/src/a00904362/ApplicationException.java
@@ -0,0 +1,44 @@
+/**
+ * Project: A00904362Lab3
+ * File: ApplicationException.java
+ * Date: Feb 09, 2016
+ * Time: 11:23:04 AM
+ */
+
+package a00904362;
+
+/**
+ * @author Alexey Gorbenko, A00904362
+ *
+ */
+
+@SuppressWarnings("serial")
+public class ApplicationException extends Exception {
+	/**
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 */
+	public ApplicationException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ApplicationException() {
+		super();
+	}
+
+	public ApplicationException(String message) {
+		super(message);
+	}
+
+	public ApplicationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ApplicationException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ A00904362Lab7/src/a00904362/Lab7.java
@@ -0,0 +1,80 @@
+/**
+ * Project: A00904362Lab1
+ * File: Lab1.java
+ * Date: Feb 09, 2016
+ * Time: 4:52:19 PM
+ */
+
+package a00904362;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import a00904362.data.Player;
+import a00904362.utils.CompareByBirthdate;
+import a00904362.utils.PlayersIO;
+
+/**
+ * @author Alexey Gorbenko, A00904362
+ *
+ */
+
+public class Lab7 {
+
+	private static int exit_code = 0;
+	private static List<Player> players;
+	private static final Logger LOG = Logger.getLogger(Lab7.class);
+	private static String FILE_LOG_SET = "log.properties";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		PropertyConfigurator.configure(FILE_LOG_SET);
+		LOG.info("Starting Lab5");
+
+		Instant firstInstant = Instant.now();
+		LOG.info(firstInstant);
+
+		try {
+
+			// if (!Validator.validate_args(args)) {
+			// throw new ApplicationException("Usage java -jar A00123456Lab2.jar <input string>");
+			// }
+
+			players = PlayersIO.populatePlayers();
+			LOG.info("End PlayersIO.populatePlayers");
+
+			Collections.sort(players, new CompareByBirthdate());
+
+			PlayersIO.displayPlayers(players);
+			LOG.info("End PlayersIO.displayPlayers");
+
+		} catch (ApplicationException e) {
+			// System.err.println(e.getMessage());
+			LOG.error(e);
+			exit_code = -1;
+		} catch (Exception e) {
+			// e.printStackTrace();
+			LOG.error(e);
+			exit_code = -1;
+		}
+
+		Instant secondInstant = Instant.now();
+		// System.out.println(secondInstant);
+		LOG.info(secondInstant);
+		// System.out.println(Duration.between(firstInstant, secondInstant).toMillis() + " ms");
+		LOG.info(Duration.between(firstInstant, secondInstant).toMillis() + " ms");
+
+		LOG.info("End Lab5. exit_code " + exit_code);
+
+		System.exit(exit_code);
+	}
+
+}
