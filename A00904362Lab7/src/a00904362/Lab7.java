57115affe529dbd69c6fea0b45bb89599bdd1630
AlexeyGorbenko <algorenator@gmail.com>
2016-03-01 17:21:32 -0800

    finish 1 march

--- A00904362Lab7/src/a00904362/Lab7.java
+++ A00904362Lab7/src/a00904362/Lab7.java
@@ -11,8 +11,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.time.Instant;
@@ -43,15 +41,11 @@ public class Lab7 {
 	private static String FILE_LOG_SET = "log.properties";
 
 	private static Properties _properties;
-	private static Connection _connection;
 	private static Database database;
 
-	public static final String DB_PROPERTIES_FILENAME = "db.properties";
+	private static final String DB_PROPERTIES_FILENAME = "db.properties";
 
-	public static final String DB_DRIVER_KEY = "db.driver";
-	public static final String DB_URL_KEY = "db.url";
-	public static final String DB_USER_KEY = "db.user";
-	public static final String DB_PASSWORD_KEY = "db.password";
+	private static final String LAB_NO = "Lab7";
 
 	/**
 	 * @param args
@@ -59,7 +53,7 @@ public class Lab7 {
 	public static void main(String[] args) {
 
 		PropertyConfigurator.configure(FILE_LOG_SET);
-		LOG.info("Starting Lab5");
+		LOG.info("Starting " + LAB_NO);
 
 		Instant firstInstant = Instant.now();
 		LOG.info(firstInstant);
@@ -91,71 +85,48 @@ public class Lab7 {
 		}
 
 		Instant secondInstant = Instant.now();
-		// System.out.println(secondInstant);
 		LOG.info(secondInstant);
-		// System.out.println(Duration.between(firstInstant, secondInstant).toMillis() + " ms");
 		LOG.info(Duration.between(firstInstant, secondInstant).toMillis() + " ms");
 
-		LOG.info("End Lab5. exit_code " + exit_code);
+		database.shutdown();
 
+		LOG.info(database.toString() + " stopped");
+		LOG.info("End " + LAB_NO + ". exit_code " + exit_code);
 		System.exit(exit_code);
 	}
 
-	/**
-	 * @throws SQLException
-	 * @throws ClassNotFoundException
-	 * @throws ApplicationException
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 * 
-	 */
-	private static void PopulateDB()
-			throws ClassNotFoundException, SQLException, ApplicationException, FileNotFoundException, IOException {
+	private static void PopulateDB() throws ClassNotFoundException, SQLException, ApplicationException,
+			FileNotFoundException, IOException, NotFoundException {
 		File file = new File(DB_PROPERTIES_FILENAME);
 		if (!file.exists()) {
 			throw new ApplicationException("File DB properties " + DB_PROPERTIES_FILENAME + " doesnt exist");
 		}
 
-		try {
-			_properties = new Properties();
-			_properties.load(new FileInputStream(file));
+		_properties = new Properties();
+		_properties.load(new FileInputStream(file));
+		database = new Database(_properties);
+		database.getConnection();
+		PlayerDao playerDao = new PlayerDao(database);
 
-			database = new Database(_properties);
-			PlayerDao playerDao = new PlayerDao(database);
-
-			Connection connection = database.getConnection();
-
-			if (!Database.tableExists(connection, PlayerDao.TABLE_NAME)) {
-				LOG.info("Table " + PlayerDao.TABLE_NAME + " doesnt exists");
-				playerDao.create();
-				LOG.info("Table " + PlayerDao.TABLE_NAME + " created");
-				playerDao.fillPlayers(players);
-			} else {
-				LOG.info("Table " + PlayerDao.TABLE_NAME + " exists");
-				// playerDao.drop(); // for testing
-				// LOG.info("Table " + PlayerDao.TABLE_NAME + " dropped"); // for testing
-				// playerDao.create(); // for testing
-			}
-
-			LOG.info("Table " + PlayerDao.TABLE_NAME + " contains " + playerDao.countAll() + " records");
-			LOG.info(playerDao.loadAll().toString());
-
-			// connect();
-		} finally {
-			if (_connection != null) {
-				_connection.close();
-			}
+		if (!database.tableExists(playerDao.getTableName())) {
+			LOG.info("Table " + playerDao.getTableName() + " doesnt exists");
+			playerDao.create();
+			LOG.info("Table " + playerDao.getTableName() + " created");
+			playerDao.fillPlayers(players);
+		} else {
+			LOG.info("Table " + playerDao.getTableName() + " exists");
+			// playerDao.drop(); // for testing
+			// LOG.info("Table " + PlayerDao.TABLE_NAME + " dropped"); // for testing
+			// playerDao.create(); // for testing
 		}
 
-	}
+		LOG.info("Table " + playerDao.getTableName() + " contains " + playerDao.countAll() + " records");
+		LOG.info("records- " + playerDao.loadAll().toString());
+		for (String pl : playerDao.getGamerTags()) {
+			LOG.info("players by tag - "
+					+ String.format("%-20s%-3s%-20s", pl, "-", playerDao.getPlayerByTag(pl).getFirstname()));
+		}
 
-	private static void connect() throws ClassNotFoundException, SQLException {
-		LOG.info("Driver loading");
-		Class.forName(_properties.getProperty(DB_DRIVER_KEY));
-		LOG.info("Driver loaded");
-		_connection = DriverManager.getConnection(_properties.getProperty(DB_URL_KEY),
-				_properties.getProperty(DB_USER_KEY), _properties.getProperty(DB_PASSWORD_KEY));
-		LOG.info("Database connected");
 	}
 
 }
